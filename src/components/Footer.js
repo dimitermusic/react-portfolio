74e22f84b79b0d55d416b5ee04ddfee45bfe1e01
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-20 15:13:24 -0800

    header and footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import '../styles/Footer.css'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
-function Footer () {
+
+function Footer() {
   return (
-    <p>Icons will go here</p>
+    <>
+      <div className="icons">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/dimitermusic/"
+        >
+          <div>
+            <FaLinkedin />
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/dimitermusic"
+        >
+          <div>
+            <FaGithub />
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.youtube.com/channel/UCfu37nGmXSfgaMsUS5yO85g"
+        >
+          <div>
+            <FaYoutube />
+          </div>
+        </a>
+      </div>
+    </>
   );
 }
 
