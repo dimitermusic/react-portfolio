acd3642573d9c2a4287b60e8b35913044fd9f8b1
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-21 20:38:01 -0800

    update contact styling

---
+++ src/components/Form.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import '../styles/Form.css'
+
+// Here we import a helper function that will check if the email is valid
+// import { validateEmail } from '../utils/helpers.js/';
+
+function Form() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either name, email, and message.
+    if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'message') {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // if (!validateEmail(email)) {
+    //   setErrorMessage('Email is invalid');
+    //   // We want to exit out of this code block if something is wrong so that the user can correct it
+    //   return;
+    // If successful, we want to clear out the input after registration.
+    setName('');
+    // TODO: Set the password back to an empty string after the user clicks submit
+    setEmail('');
+    setMessage('');
+    setErrorMessage('')
+    alert(`Message sent successfully!
+Thank you for your message, ${name}! I will get back to you as soon as possible.`);
+  }
+
+  return (
+    <div className="form-container">
+      <form className="form">
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        <textarea
+          rows="5"
+          cols="50"
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Write your message here...">
+        </textarea>
+        <button className="button" type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Form;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import Form from '../Form'
 import '../../styles/Contact.css'
 
 export default function Contact() {
   return (
     <div className="contact">
       <h1>Contact Me</h1>
-      <p>
-        A form will go here
-      </p>
+      <Form />
     </div>
   );
 }
