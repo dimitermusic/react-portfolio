6fda3f241720278039316335804bc136ab6c89b6
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-22 08:51:35 -0800

    portfolio page complete

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,30 +3,28 @@ import '../styles/Project.css'
 
 function Project(props) {
     return (
-        <div className="project-section">
-            <div className="project-card">
-                <img
+        <div className="project-card">
+            <img
                 src={props.image}
                 alt={props.alt}
-                />
-                <div className="project-label">
-                    <div>
-                        <h1>{props.name}</h1>
-                    </div>
-                    <div className="links">
-                        <a
-                            target="_blank"
-                            href={props.github}
-                        >
-                            Github Repository
-                        </a>
-                        <a
-                            target="_blank"
-                            href={props.deployed}
-                        >
-                            Deployed Site
-                        </a>
-                    </div>
+            />
+            <div className="project-label">
+                <div>
+                    <h1>{props.name}</h1>
+                </div>
+                <div className="links">
+                    <a
+                        target="_blank"
+                        href={props.github}
+                    >
+                        Github Repository
+                    </a>
+                    <a
+                        target="_blank"
+                        href={props.deployed}
+                    >
+                        Deployed Site
+                    </a>
                 </div>
             </div>
         </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../../styles/Portfolio.css'
 import Project from '../Project';
+import official from '../../images/official-website.png'
+import ecommerce from '../../images/ecommerce.png'
+import budget from '../../images/budget-tracker.png'
+import workout from '../../images/workout-tracker.png'
+import bebop from '../../images/bebop.png'
+import workday from '../../images/work-day-2.png'
 
 export default function Portfolio() {
   return (
@@ -11,22 +17,22 @@ export default function Portfolio() {
           name="Offical Website (HTML/CSS/Javascript"
           github="https://github.com/dimitermusic/official-website.html"
           deployed="https://www.dimitermusic.com/"
-          // image={require('../../images/official-website.png')}
-          // alt="dimiter official website"
+          image={official}
+          alt="dimiter official website"
         />
         <Project
           name="E-Commerce Backend (NodeJs/ExpressJS)"
           github="https://github.com/dimitermusic/ecommerce-backend"
           deployed="https://dy-ecommerce.herokuapp.com/"
-          // image={require('../../images/ecommerce.png')}
-          // alt="ecommerce backend"
+          image={ecommerce}
+          alt="ecommerce backend"
         />
         <Project
           name="PWA Budget Tracker (MongoDB/PWA)"
           github="https://github.com/dimitermusic/pwa-budget-tracker"
           deployed="https://dimiter-budget-tracker.herokuapp.com/"
-          // image={require('../../images/budget-tracker.png')}
-          // alt="pwa budget tracker"
+          image={budget}
+          alt="pwa budget tracker"
         />
       </div>
       <div className="project-row">
@@ -34,22 +40,22 @@ export default function Portfolio() {
           name="NoSql Workout Tracker (MongoDB/IndexedDB)"
           github="https://github.com/dimitermusic/nosql-workout-tracker"
           deployed="https://dimiter-workout-tracker.herokuapp.com/"
-          // image={require('../../images/workout-tracker.png')}
-          // alt="NoSql Workout Tracker"
+          image={workout}
+          alt="NoSql Workout Tracker"
         />
         <Project
           name="Bebop Music (MySQL/NodeJS/ExpressJS)"
           github="https://github.com/deannaboiani/bebop"
           deployed="https://bebopmusic.herokuapp.com/"
-          // image={require('../../images/bebop.png')}
-          // alt="bebop music"
+          image={bebop}
+          alt="bebop music"
         />
         <Project
           name="Work Day Planner (Javascript/HTML/CSS)"
           github="https://github.com/dimitermusic/work-day-planner.html"
           deployed="https://dimitermusic.github.io/work-day-planner.html/"
-          // image={require('../../images/work-day-2.png')}
-          // alt="work day planner"
+          image={workday}
+          alt="work day planner"
         />
       </div>
     </div>
