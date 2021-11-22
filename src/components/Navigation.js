dc9f46e63a97e6d96c5804689aecd0e8d1db40ce
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-22 11:04:01 -0800

    removed href from nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,6 @@ function Navigation({ currentPage, handlePageChange }) {
       <ul className="nav">
         <li className="nav-item">
           <a
-            href="#AboutMe"
             onClick={() => handlePageChange('AboutMe')}
             // This is a conditional (ternary) operator that checks to see if the current page is "AboutMe"
             // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -21,7 +20,6 @@ function Navigation({ currentPage, handlePageChange }) {
         </li>
         <li className="nav-item">
           <a
-            href="#Portfolio"
             onClick={() => handlePageChange('Portfolio')}
             // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
@@ -31,7 +29,6 @@ function Navigation({ currentPage, handlePageChange }) {
         </li>
         <li className="nav-item">
           <a
-            href="#Contact"
             onClick={() => handlePageChange('Contact')}
             // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
@@ -41,7 +38,6 @@ function Navigation({ currentPage, handlePageChange }) {
         </li>
         <li className="nav-item">
           <a
-            href="#Resume"
             onClick={() => handlePageChange('Resume')}
             // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
