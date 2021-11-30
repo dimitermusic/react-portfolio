4a9deb2e5be8d21b05e87070c19229362f0ea0b8
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-30 03:05:16 -0800

    style updates

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,36 +7,38 @@ export default function Resume() {
       <h1>
         Resume
       </h1>
-      <h4>
-        Proficiencies:
-      </h4>
-      <ul>
-        <li>Javascript</li>
-        <li>ReactJS</li>
-        <li>NodeJS</li>
-        <li>ExpressJS</li>
-        <li>Angular</li>
-        <li>MySQL</li>
-        <li>MongoDB</li>
-        <li>Bootstrap</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>RESTful APIs</li>
-        <li>MVC Pattern</li>
-        <li>Responsive/Mobile-First Design</li>
-        <li>Project Management</li>
-        <li>Github</li>
-        <li>Agile</li>
-        <li>SEO</li>
-        <li>Marketing</li>
-      </ul>
+      <div className="resProf">
+        <h5>
+          Proficiencies:
+        </h5>
+        <ul>
+          <li>Javascript</li>
+          <li>ReactJS</li>
+          <li>NodeJS</li>
+          <li>ExpressJS</li>
+          <li>Angular</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>Bootstrap</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>RESTful APIs</li>
+          <li>MVC Pattern</li>
+          <li>Responsive/Mobile-First Design</li>
+          <li>Project Management</li>
+          <li>Github</li>
+          <li>Agile</li>
+          <li>SEO</li>
+          <li>Marketing</li>
+        </ul>
+      </div>
       <div className="resume-link">
         <p>
           <a
             target="_blank"
             href="https://drive.google.com/file/d/1VMOJwObY6hqVxgulrWM4l4Jfo-3diUzT/view?usp=sharing"
             rel="noreferrer"
-            >
+          >
             Click Here
           </a>
           {' '}to view and download my resume.
