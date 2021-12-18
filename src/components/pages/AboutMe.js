b1ebf848bec42cb79ec69c3b8fbf94aa7cd6e0f7
dimitermusic <dimiteryyordanov@gmail.com>
2021-12-18 10:05:25 -0800

    updated about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -10,7 +10,7 @@ export default function AboutMe() {
       </h1>
       <img src={dimiterProfile} alt="dimiter yordanov" />
       <p>
-        Full Stack Web Developer with strong storytelling and problem-solving abilities. Notable history embodying the essence of stories and visuals through creating music for Netflix, Hulu, Prime Video, NEON, and more. Eager to leverage my extensive experience in Film and TV, Communications, Team Management, and Web Design, Development and Testing to join and collaborate with a diverse team of engineers in order to learn and grow as a Web Developer, making an impact in the world of mobile user experience.
+        Full Stack Web Developer with strong storytelling and problem-solving abilities. Notable history embodying the essence of stories and visuals, creating music for Netflix, Hulu, Prime Video, NEON, and more. Eager to offer my unique perspective and extensive experience in Film and TV, Communications, Team Management, and Web Development by joining and collaborating with a diverse and passionate team of engineers in order to learn and grow as a Web Developer, making an impact in the world of user experience.
       </p>
     </div>
   );
