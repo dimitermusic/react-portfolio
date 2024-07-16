80897b4bb0e354f70bc083a3afe4c6b98dee3780
Dimiter Yordanov <dyordanov@qualtrics.com>
2024-07-15 22:07:43 -0700

    minor fix

--- src/components/Page.js
+++ src/components/Page.js
@@ -33,7 +33,7 @@ export default function Page() {
           <iframe src='https://player.vimeo.com/video/926709263?h=657ef3fd79&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479' width={window.innerWidth > 960 ? '960' : '336'} height={window.innerWidth > 960 ? '540' : '189'} frameborder='0' allow='autoplay; fullscreen; picture-in-picture; clipboard-write' title='Dimiter Yordanov Music Composer Demo Reel'></iframe>
         </div>
         <div style={{marginTop: '20px'}}>
-          <iframe src='https://player.vimeo.com/video/984691330?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479' width={window.innerWidth > 960 ? '960' : '336'} height={window.innerWidth > 960 ? '540' : '189'} frameborder='0' allow='autoplay; fullscreen; picture-in-picture; clipboard-write' title='Dimiter Yordanov Music Composer Demo Reel'></iframe>
+          <iframe src='https://player.vimeo.com/video/984691330?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479' width={window.innerWidth > 960 ? '960' : '336'} height={window.innerWidth > 960 ? '540' : '189'} frameborder='0' allow='autoplay; fullscreen; picture-in-picture; clipboard-write' title='Dimiter Yordanov Summer Mix Demo Reel'></iframe>
         </div>
         <h2 id='audio' style={{marginTop: '200px'}}>Audio</h2>
         <div style={{marginTop: '20px'}}>
