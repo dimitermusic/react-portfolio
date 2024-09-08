485c17ad926358e607cc24b106c3b56d25e34789
Dimiter Yordanov <dimitermusic@gmail.com>
2024-09-08 12:35:59 -0700

    reverting

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,13 +6,13 @@ export default function Resume() {
     <div className='resume'>
       <h1>Resume</h1>
       <div className='skills'>
-        <h5>Proficiencies:</h5>
+        <h5>Technical Proficiencies:</h5>
         <p>
-          Extensive experience in application security, team leadership, software design, and maintenance of complex legacy systems processing millions of sensitive records safely. My technical proficiency spans a range of technologies and methodologies, including React, TypeScript, Node.js, Python, MySQL, MongoDB, Git, AWS, Jira, Test Driven Development (TDD), and Agile practices. I have a proven track record in delivering products and features to Fortune 50 enterprises, consistently meeting deadlines and often completing projects up to two weeks ahead of schedule. I spearheaded a comprehensive overhaul of our organization's security operations. This initiative involved personally designing, building, testing, and launching an innovative automation solution. The project was part of a broader strategy to address existing gaps in the workload and timeline management of our application security remediations.
+          Extensive experience in application security, team leadership, software design, and maintenance of complex legacy systems processing millions of sensitive records securely. My technical proficiency spans a range of technologies and methodologies, including React, TypeScript, Node.js, Python, MySQL, MongoDB, Git, AWS, Jira, Test Driven Development (TDD), and Agile practices. I have a proven track record in delivering solutions to enterprise customers, consistently hitting deadlines. I spearheaded a comprehensive overhaul of our organization's security operations. This initiative involved personally designing, building, testing, and launching an innovative automation solution. The project was part of a broader strategy to address existing gaps in the workload and timeline management of our application security remediations.
         </p>
       </div>
       <div className='skills'>
-        <h5>Soft Skills:</h5>
+        <h5>Personal Attributes:</h5>
         <p>
           Unusually determined and lightning-fast learner at the core, always
           curious, genuinely excited to come to work five days per week and
@@ -25,7 +25,7 @@ export default function Resume() {
         <p>
           <a
             target='_blank'
-            href='https://drive.google.com/file/d/1zF_lNw_gk0D5YwerTom6BLdhBx_vjF25/view?usp=sharing'
+            href='https://drive.google.com/file/d/1pQwcHFVcnfVpBrLwGh8wZqT_i_rHvNoR/view?usp=sharing'
             rel='noreferrer'
           >
             Click Here
